88276dc336b5d65ad2de21813d0ef768666abfec
Nguyen Thanh Tung <nguyenthanhtungclc@gmail.com>
2023-12-11 19:08:16 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,45 @@
+let arr = [];
+
+var row = document.querySelectorAll("#id tbody tr");
+// read tags td in row
+for (var i = 0; i < row.length; i++) {
+  var td = row[i].querySelectorAll("td");
+  let firstName = td[5].textContent;
+  firstName = toNonAccentVietnamese(firstName);
+  let lastName1 = td[3].textContent.substring(0, 1);
+  lastName1 = toNonAccentVietnamese(lastName1);
+  let midName = td[4].textContent;
+  midName = toNonAccentVietnamese(midName);
+  let split = midName.split(" ");
+  midName = "";
+  for (let j = 0; j < split.length; j++) {
+    midName += split[j].substring(0, 1);
+  }
+  let id = td[2].textContent;
+  let email = firstName + lastName1 + midName + id + "@fpt.edu.vn";
+  arr.push(email);
+}
+
+console.log(arr);
+
+// This function keeps the casing unchanged for str, then perform the conversion
+function toNonAccentVietnamese(str) {
+  str = str.replace(/A|Á|À|Ã|Ạ|Â|Ấ|Ầ|Ẫ|Ậ|Ă|Ắ|Ằ|Ẵ|Ặ/g, "A");
+  str = str.replace(/à|á|ạ|ả|ã|â|ầ|ấ|ậ|ẩ|ẫ|ă|ằ|ắ|ặ|ẳ|ẵ/g, "a");
+  str = str.replace(/E|É|È|Ẽ|Ẹ|Ê|Ế|Ề|Ễ|Ệ/, "E");
+  str = str.replace(/è|é|ẹ|ẻ|ẽ|ê|ề|ế|ệ|ể|ễ/g, "e");
+  str = str.replace(/I|Í|Ì|Ĩ|Ị/g, "I");
+  str = str.replace(/ì|í|ị|ỉ|ĩ/g, "i");
+  str = str.replace(/O|Ó|Ò|Õ|Ọ|Ô|Ố|Ồ|Ỗ|Ộ|Ơ|Ớ|Ờ|Ỡ|Ợ/g, "O");
+  str = str.replace(/ò|ó|ọ|ỏ|õ|ô|ồ|ố|ộ|ổ|ỗ|ơ|ờ|ớ|ợ|ở|ỡ/g, "o");
+  str = str.replace(/U|Ú|Ù|Ũ|Ụ|Ư|Ứ|Ừ|Ữ|Ự/g, "U");
+  str = str.replace(/ù|ú|ụ|ủ|ũ|ư|ừ|ứ|ự|ử|ữ/g, "u");
+  str = str.replace(/Y|Ý|Ỳ|Ỹ|Ỵ/g, "Y");
+  str = str.replace(/ỳ|ý|ỵ|ỷ|ỹ/g, "y");
+  str = str.replace(/Đ/g, "D");
+  str = str.replace(/đ/g, "d");
+  // Some system encode vietnamese combining accent as individual utf-8 characters
+  str = str.replace(/\u0300|\u0301|\u0303|\u0309|\u0323/g, ""); // Huyền sắc hỏi ngã nặng
+  str = str.replace(/\u02C6|\u0306|\u031B/g, ""); // Â, Ê, Ă, Ơ, Ư
+  return str;
+}
